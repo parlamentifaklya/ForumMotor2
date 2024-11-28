8832cad2f7055e65bc26c04b7489baab3826943e
parlamentifaklya <20.kokenyesi.andor@mechwart.com>
2024-11-28 10:48:40 +0100

    Add project files.

---
+++ ForumMotor_13BC_A/Pages/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ForumMotor_13BC_A.Data;
+using ForumMotor_13BC_A.Models;
+
+namespace ForumMotor_13BC_A.Pages
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ForumMotor_13BC_A.Data.ApplicationDbContext _context;
+
+        public CreateModel(ForumMotor_13BC_A.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return Page();
+        }
+
+        [BindProperty]
+        public Category Category { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            /*if (!ModelState.IsValid)
+            {
+                return Page();
+            }*/
+
+            _context.Categories.Add(Category);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
